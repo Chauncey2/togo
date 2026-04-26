00b0017baa7b6bc7141140c6c9c28f1ba07a85b0
gaochangju <gaochangju@nami.com>
2026-04-26 19:53:46 +0800

    init

---
+++ exercises/day01/main.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+/*
+学习内容：
+包、模块、变量、常量、函数、控制流
+go run、go build、go fmt
+
+Go 项目的最小结构
+输出物：
+写一个 CLI 小程序：接收姓名、年龄、城市参数，输出格式化结果
+写一段学习笔记：Go 的包和 Python module 有什么不同
+
+*/
+
+func test1(){
+	fmt.Println("test")
+}
+
+func main(){
+	test1()
+}
